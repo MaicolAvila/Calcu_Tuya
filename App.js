2a9d7c1ff292839588d5c6284073eadd7438952f
Maicol <maavilav@correo.udistrital.edu.co>
2020-04-22 17:19:31 -0500

    commit2

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Calcu_Tuya</Text>
+      <Text>Hello world</Text>
+      
+      
     </View>
   );
 }
